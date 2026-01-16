6ade2c14db39528040a7a5a8186c9a9c4594b859
Warren <warrendsteinacker@gmail.com>
2026-01-16 08:05:02 -0500

    adding routes and making new comps

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import axios from 'axios'
-import {useNavigate} from 'react-router-dom'
+import Loginp from './c:/Users/User/llm/vite-project/src/Loginp'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   
-  const [pass, setP] = useState('')
-  const nav = useNavigate()
-   
-  const loginf = async(e) => {
-    e.preventDefault()
-
-    try{
-        e.preventDefault()
-        const d = await axios.post('http://localhost:8000/login', {password: pass})
-        const res = d.data
-        const R = res.role 
-        localStorage.setItem('role', R)
-        nav('/H')
-      }
-      catch(error){
-        console.error(error.message)
-      }
-
-  }
-
   return (
     <>
-    <form onSubmit={loginf}>
-      <input type='password' onChange={(e)=> setP(e.target.value)}></input>
-      <button type='submit'>post</button>
-    </form>
+    <Routes>
+      <Route path='/' element={<Loginp/>}/>
+      <Route path='/H' element={<Home/>}/>
+    </Routes>
     </>
   )
 }
--- vite-project/src/loginp.jsx
+++ vite-project/src/loginp.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import {useNavigate} from 'react-router-dom'
 
-function App() {
+function Loginp() {
   
   const [pass, setP] = useState('')
   const nav = useNavigate()
@@ -34,4 +34,4 @@ function App() {
   )
 }
 
-export default App
+export default Loginp
