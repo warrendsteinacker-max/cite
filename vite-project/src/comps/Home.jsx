49d676460d05ba74c293703aec05c7cf3ecfdb97
Warren <warrendsteinacker@gmail.com>
2026-01-16 11:08:04 -0500

    got it working

--- vite-project/src/comps/Home.jsx
+++ vite-project/src/comps/Home.jsx
@@ -3,9 +3,9 @@ import React, {useState} from 'react'
 const Home = () => {
 
     const RRR = localStorage.getItem('role')
-    const [sent, setS] = useState(false)
-    const [M, setM] = useState('')
-    const [pass, setPass] = useState('')
+    // const [sent, setS] = useState(false)
+    // const [M, setM] = useState('')
+    // const [pass, setPass] = useState('')
     const [count, setC] = useState(0)
     const [data, setData] = useState([])
     // const [E, setE] = useState(false)
@@ -25,32 +25,28 @@ const Home = () => {
        }
     }
 
-    const Changep = async(e) => {
-        e.preventDefault()
-        try{
-            const feedback = await axios.put('http://localhost:8000/edit', {pass: pass, RRR: RRR})
-            const res = feedback.data
-            const fB = res.sent 
-            localStorage.setItem('feedback', fB)
-            setS(true)
-            setM(fB)
-        }
-        catch(error){
-            console.error(error.message)
-            setS(false)
-        }
-    }
+    // const Changep = async(e) => {
+    //     e.preventDefault()
+    //     try{
+    //         const feedback = await axios.put('http://localhost:8000/edit', {pass: pass, RRR: RRR})
+    //         const res = feedback.data
+    //         const fB = res.sent 
+    //         localStorage.setItem('feedback', fB)
+    //         setS(true)
+    //         setM(fB)
+    //     }
+    //     catch(error){
+    //         console.error(error.message)
+    //         setS(false)
+    //     }
+    // }
 
   return (
     <>
-    {sent ? (<p>{M}</p>):(<p>{M}</p>)}
+    {/* {sent ? (<p>{M}</p>):(<p>{M}</p>)} */}
     <p>Welcom {RRR}</p>
-    <form onSubmit={Changep}>
-        <input type='password' value={pass} onChange={(e)=>setPass(e.target.value)}></input>
-        <button type='submit'>change p</button>
-        <button onClick={fetchd}> fetch d </button>
-        <div style={{display: flex, justifyContent: center, alignItems: center}}><button onClick={fetchd}>fetch d</button><div>{data.map((item)=>{<p></p>})}</div></div>
-    </form>
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}><button onClick={fetchd}>fetch d</button><div>{data.map((item)=>{return <p key={item.id}>{item.id}</p>})}</div></div>
+    <button onClick={fetchd}> fetch d </button>
     </>
   )
 
@@ -58,4 +54,9 @@ const Home = () => {
 
 export default Home
 
-/////<div style={{display: flex, justifyContent: center, alignItems: center}}><button onClick={fetchd}>fetch d</button><div>{data.map((item)=>{<p></p>})}</div>
+/////<div style={{display: flex, justifyContent: center, alignItems: center}}><button onClick={fetchd}>fetch d</button><div>{data.map((item)=>{<p>{item.id}</p>})}</div></div>
+    // <form onSubmit={Changep}>
+    //     <input type='password' value={pass} onChange={(e)=>setPass(e.target.value)}></input>
+    //     <button type='submit'>change p</button>
+    //     <button onClick={fetchd}> fetch d </button>
+    // </form>
